7a6bce49611d6ff5c6bf1d6baf99c5fc8573306f
chanisaulkarimah <chanisaulkarimah27@gmail.com>
2023-06-08 23:59:23 +0700

    label notif

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import sqlite3
 from getpass import getpass
-from task_interface import create_task_interface
+from display import create_task_interface
 
 # Membuat koneksi ke database SQLite
 conn = sqlite3.connect("task_manager.db")
@@ -22,25 +22,35 @@ def login():
         create_task_interface(conn)
     else:
         # Menampilkan pesan kesalahan jika login gagal
-        label_login_error = tk.Label(window_login, text="Username atau password salah.")
+        label_login_error = tk.Label(window_login, text="Username atau password salah.", font=("Cabin", 12), foreground="black")
         label_login_error.pack()
 
 # Membuat jendela Tkinter untuk login
 window_login = tk.Tk()
 window_login.title("Login - Pengelola Tugas")
 
-label_username = tk.Label(window_login, text="Username:")
+# Menambahkan gambar pada jendela login
+photo = tk.PhotoImage(file="logo.jpg")
+label = tk.Label(image=photo)
+label.image = photo # must keep a reference, otherwise it will be garbage collected
+label.pack()
+
+# Menyesuaikan tampilan latar belakang dan font pada jendela login
+window_login.configure(bg="#F9C784")
+tk.Label(window_login, text="Masuk ke Pengelola Tugas", bg="#F9C784", fg="#ffffff", font=("Helvetica", 20)).pack(pady=10)
+
+label_username = tk.Label(window_login, text="Username:", bg="#F9C784", fg="#ffffff", font=("Helvetica", 14))
 label_username.pack()
-entry_username = tk.Entry(window_login)
+entry_username = tk.Entry(window_login, font=("Helvetica", 14))
 entry_username.pack()
 
-label_password = tk.Label(window_login, text="Password:")
+label_password = tk.Label(window_login, text="Password:", bg="#F9C784", fg="#ffffff", font=("Helvetica", 14))
 label_password.pack()
-entry_password = tk.Entry(window_login, show="*")
+entry_password = tk.Entry(window_login, show="*", font=("Helvetica", 14))
 entry_password.pack()
 
-button_login = tk.Button(window_login, text="Login", command=login)
-button_login.pack()
+button_login = tk.Button(window_login, text="Login", command=login, bg="#065efb", fg="#ffffff", font=("Helvetica", 14))
+button_login.pack(pady=10)
 
 # Menjalankan aplikasi login
 window_login.mainloop()
